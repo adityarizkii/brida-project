b8863c03187e5777e036c02e8a2bb6741cb79af1
Aditya Rizki <aditya4rizki@gmail.com>
2023-11-19 18:55:00 +0800

    connect database

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/lib/prisma";
 
-type Data = {
-  name: string
+async function handleGetMethod(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const result = await prisma.user.findMany();
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ message: "error when try to get data" });
+  }
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+async function handlePostMethod(req: NextApiRequest, res: NextApiResponse) {
+  try {
+  } catch (error) {}
 }
+
+function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    handleGetMethod(req, res);
+  } else if (req.method === "POST") {
+    handlePostMethod(req, res);
+  }
+}
+
+export default handler;
---
+++ src/pages/tescrud/index.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+
+type User = {
+  id: string;
+  name: string;
+  age: number;
+  createdAt: Date;
+  updatedAt: Date;
+}[];
+
+const index = () => {
+  const [dataJSON, setDataJSON] = useState<User>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/hello");
+        const result = await response.json();
+
+        setDataJSON(result);
+      } catch (error) {
+        console.log("error fetching data", error);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="my-4 text-center text-xl font-medium">database request</h1>
+
+      <table className="mx-auto w-[300px] overflow-hidden rounded-md border bg-gray-100">
+        <thead className="bg-gray-700 text-gray-200">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Name
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Age
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataJSON?.map((data) => (
+            <tr className="border-b border-gray-700" key={data.id}>
+              <td className="px-6 py-3">{data.name}</td>
+              <td className="px-6 py-3 text-center">{data.age}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default index;
